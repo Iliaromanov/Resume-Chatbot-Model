3fa8060283c4718d546721df1c0c73b351f8c019
Iliaromanov <iliaromanov033@gmail.com>
2021-04-03 22:39:45 -0400

    parsing intents file

--- main.py
+++ main.py
@@ -12,4 +12,15 @@ import json
 with open('intents.json') as file:
     data = json.load(file)
 
+words = []
+labels = []
+docs = []
 
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern) # breaks up string into list of words
+        words.extend(wrds) # extend() adds all elements of an iterable to the end of the list
+        docs.append(pattern)
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
