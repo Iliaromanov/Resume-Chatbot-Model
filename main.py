eab5b46bc010c9c38333e84dad73ccf0160dd4ea
Iliaromanov <iliaromanov033@gmail.com>
2021-04-04 21:49:56 -0400

    switching to jupyter notebook

--- main.py
+++ main.py
@@ -12,15 +12,23 @@ import json
 with open('intents.json') as file:
     data = json.load(file)
 
-words = []
-labels = []
-docs = []
+words = [] # possible unique words
+labels = [] # possible unique labels (tags)
+docs_x = [] # Pattern inputs
+docs_y = [] # Corresponding tags for docs_x patterns
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern) # breaks up string into list of words
         words.extend(wrds) # extend() adds all elements of an iterable to the end of the list
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
 
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
+
+# Stem words and convert to lowercase
+words = [stemmer.stem(w.lower()) for w in words]
+# Use set to remove duplicates and sort
+words = sorted(list(set(words)))
+labels = sorted(labels)
