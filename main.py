2e3f1ea2af4a1d95f6e685737d0f4be0ab3f795c
Iliaromanov <iliaromanov033@gmail.com>
2021-04-26 16:11:00 -0400

    skipping data preprocessing and model training if model already exists

--- main.py
+++ main.py
@@ -1,34 +1,92 @@
 import nltk
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tflearn
+import tensorflow as tf
+import random
+import json
+import pickle
+
+# Prepare a stemmer
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy
-import tflearn
-import tensorflow
-import random
-import json
-
-
+# Load the data
 with open('intents.json') as file:
     data = json.load(file)
 
-words = [] # possible unique words
-labels = [] # possible unique labels (tags)
-docs_x = [] # Pattern inputs
-docs_y = [] # Corresponding tags for docs_x patterns
+# Don't parse/preprocess data if already done before
+try:
+    with open('data.pickle', 'rb') as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = [] # possible unique words
+    labels = [] # possible unique labels (tags)
+    docs_x = [] # Pattern inputs
+    docs_y = [] # Corresponding tags for docs_x patterns
 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern) # breaks up string into list of words
-        words.extend(wrds) # extend() adds all elements of an iterable to the end of the list
-        docs_x.append(pattern)
-        docs_y.append(intent["tag"])
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            pattern_wrds = nltk.word_tokenize(pattern) # breaks up string into list of words
+            words.extend(pattern_wrds) # extend() adds all elements of an iterable to the end of the list
+            docs_x.append(pattern_wrds) # If a pattern > one word then stemmer will just lowercase it
+            docs_y.append(intent["tag"])
 
-    if intent["tag"] not in labels:
-        labels.append(intent["tag"])
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
 
-# Stem words and convert to lowercase
-words = [stemmer.stem(w.lower()) for w in words]
-# Use set to remove duplicates and sort
-words = sorted(list(set(words)))
-labels = sorted(labels)
+    # Stem words and convert to lowercase
+    words = [stemmer.stem(w) for w in words]
+    # Use set to remove duplicates and then sort
+    words = sorted(list(set(words)))
+    labels = sorted(labels)
+
+    # Using one-hot encoding to prepare data for model
+    training = []
+    output = []
+    out_empty = [0 for _ in range(len(labels))]
+
+    for i, doc in enumerate(docs_x):
+        bag = []
+
+        # Stem each word in pattern and remove common punctuation symbols
+        wrds = [stemmer.stem(w) for w in doc if w not in "?!.,'"]
+
+        for w in words:
+            if w in doc:
+                bag.append(1) # put 1 if word is in pattern
+            else:
+                bag.append(0) # 0 if not
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[i])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    # Convert lists into numpy arrays so that they can be given as inputs to the model
+    training = np.array(training)
+    output = np.array(output)
+
+# Save the training and test data
+with open('data.pickle', 'wb') as f:
+    pickle.dump((words, labels, training, output), f)
+
+# Building Neural Net
+# Input layer has len(x[0]) neurons (# of unique words)
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8) # hidden layer with 8 neurons
+net = tflearn.fully_connected(net, 8) # hidden layer with 8 neurons
+# Output layer has len(Y[0]) neurons (# of unique pattern tags/labels)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+# Don't train model if it already exists
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=780, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
