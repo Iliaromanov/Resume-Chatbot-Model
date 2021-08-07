26e2b370e939c9e6dcae12086529e969afdc60dd
Iliaromanov <iliaromanov033@gmail.com>
2021-08-06 22:10:11 -0400

    added conditions to nltk_POS_lemmatizer pipeline

--- nlp_pipelines.py
+++ nlp_pipelines.py
@@ -38,4 +38,5 @@ def nltk_POS_lemmatizer(sentence: str) -> List[str]:
     tokens = nltk.word_tokenize(sentence)
     token_tag_pairs = nltk.pos_tag(tokens)
 
-    return [lemmatizer.lemmatize(token[0], tag_dict.get(token[1][0], wordnet.NOUN)) for token in token_tag_pairs]
+    return [lemmatizer.lemmatize(token[0], tag_dict.get(token[1][0], wordnet.NOUN)).lower() 
+            for token in token_tag_pairs if token[0] not in "?!,."]
