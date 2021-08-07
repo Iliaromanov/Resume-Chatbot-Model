717b851387c9bd37a20b6fb3e0976931be3d2a25
Iliaromanov <iliaromanov033@gmail.com>
2021-08-07 17:51:20 -0400

    updated intents, re-trained model, showing top 3 predictions in CLI

--- main.py
+++ main.py
@@ -4,13 +4,30 @@ import numpy as np
 import pickle
 import json
 
+from tensorflow.python.ops.gen_array_ops import reverse
+
 from nlp_pipelines import nltk_POS_lemmatizer
 
-model_path = './sgd_model'
+model_path = 'adam_model'
 model = tf.keras.models.load_model(model_path)
 
-words = pickle.load(open('sgd_model/words.pkl', 'rb'))
-classes = pickle.load(open('sgd_model/classes.pkl', 'rb'))
+words = pickle.load(open(f'{model_path}/words.pkl', 'rb'))
+classes = pickle.load(open(f'{model_path}/classes.pkl', 'rb'))
+
+def main():
+    done = False
+    while not done:
+        sentence = input("Talk: ")
+        if sentence.lower() == "exit":
+            done = True
+        else:
+            bag = bag_words(sentence, words)
+            result = model.predict(np.array([bag]))[0]
+            probs = {classes[i]: prob for i, prob in enumerate(result)}
+            probs_top_three = {k: f"{v*100:,.2f}%" for k, v in sorted(probs.items(), key=lambda x: x[1], reverse=True)[:3]}
+            print(probs_top_three)
+            print(classes[np.argmax(result)])
+
 
 def bag_words(sentence, known_words):
     bag = [0] * len(known_words)
@@ -24,15 +41,6 @@ def bag_words(sentence, known_words):
 
     return np.array(bag)
 
-done = False
-while not done:
-    sentence = input("Talk: ")
-    if sentence.lower() == "exit":
-        done = True
-    else:
-        bag = bag_words(sentence, words)
-        result = model.predict(np.array([bag]))[0]
-        probabilities = {classes[i]: f"{prob*100:,.2f}%" for i, prob in enumerate(result)}
-        print(probabilities)
-        print(classes[np.argmax(result)])
 
+if __name__ == "__main__":
+    main()
