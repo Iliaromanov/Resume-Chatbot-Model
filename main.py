a5ebdc136c0a3ece4e9fb00e31e2ea086687d9cd
Iliaromanov <iliaromanov033@gmail.com>
2021-08-07 14:30:09 -0400

    chatbot CLI, using model

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
+import pickle
 import tensorflow as tf
-from tensorflow.keras import layers
 import numpy as np
+import pickle
+import json
 
-import nltk
-from nltk.stem import WordNetLemmatizer
+from nlp_pipelines import nltk_POS_lemmatizer
+
+model_path = './sgd_model'
+model = tf.keras.models.load_model(model_path)
+
+words = pickle.load(open('sgd_model/words.pkl', 'rb'))
+classes = pickle.load(open('sgd_model/classes.pkl', 'rb'))
+
+def bag_words(sentence, known_words):
+    bag = [0] * len(known_words)
+
+    word_pattern = nltk_POS_lemmatizer(sentence)
+
+    for new_word in word_pattern:
+        for i, word in enumerate(known_words):
+            if new_word == word:
+                bag[i] = 1
+
+    return np.array(bag)
+
+done = False
+while not done:
+    sentence = input("Talk: ")
+    if sentence.lower() == "exit":
+        done = True
+    else:
+        bag = bag_words(sentence, words)
+        result = model.predict(np.array([bag]))[0]
+        probabilities = {classes[i]: f"{prob*100:,.2f}%" for i, prob in enumerate(result)}
+        print(probabilities)
+        print(classes[np.argmax(result)])
+
