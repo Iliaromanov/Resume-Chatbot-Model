3d7d717f0de61468b71107014db34931674750ba
Iliaromanov <iliaromanov033@gmail.com>
2021-08-18 14:04:46 -0400

    new DNN model; updated intents.json

--- main.py
+++ main.py
@@ -30,6 +30,11 @@ def main():
     words_2 = pickle.load(open(rf'{model_path_2}\svm_words.pkl', 'rb'))
     classes_2 = pickle.load(open(rf'{model_path_2}\svm_classes.pkl', 'rb'))
 
+    keras_model_path = "keras_DNN_model"
+    keras_model = tf.keras.models.load_model(keras_model_path)
+    keras_words = pickle.load(open(rf'{keras_model_path}\words.pkl', 'rb'))
+    keras_classes = pickle.load(open(rf'{keras_model_path}\classes.pkl', 'rb'))
+
     done = False
     while not done:
         sentence = input("Talk: ")
@@ -44,6 +49,13 @@ def main():
             probs = {classes[i]: prob for i, prob in enumerate(result)}
             probs_top_three = {k: f"{v*100:,.2f}%" for k, v in sorted(probs.items(), key=lambda x: x[1], reverse=True)[:3]}
             print(f"DNN Prediction: {probs_top_three} => {classes[np.argmax(result)]}")
+
+            # newest trained keras model. The naming is pretty bad cus adam_model_2 is also a keras model, its just an older version
+            keras_bag = bag_words(sentence, keras_words)
+            result = keras_model.predict(np.array([keras_bag]))[0]
+            probs = {keras_classes[i]: prob for i, prob in enumerate(result)}
+            probs_top_three = {k: f"{v*100:,.2f}%" for k, v in sorted(probs.items(), key=lambda x: x[1], reverse=True)[:3]}
+            print(f"Newest DNN Prediction (this one is used in the web app): {probs_top_three} => {keras_classes[np.argmax(result)]}")
 
 
 def bag_words(sentence, known_words):
@@ -65,4 +77,5 @@ if __name__ == "__main__":
 
 
 # "Whats that AI project you did" classifies as AI-Based-Desktop-Controller by DNN and greeting by SVM
-# "How can you help me" classifies as Goodbye 73%, about chat-bot 10% by DNN and about chat-bot by SVM
+# "How can you help me" classifies as Goodbye 73%, about chat-bot 10% by DNN and about chat-bot by SVM, newest DNN gets it right
+# "I have to get going" only classfies as goodbye in newest DNN, svm and adam classify it as Golang-Blockchain project
